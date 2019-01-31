773a6cdae7801005feaa34d16edbba0a2644f523
buffalorob <peckham.rob@gmail.com>
2019-01-31 09:34:38 -0500

    routes: + profiles to namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :profiles
+  
   namespace :api do
     namespace :v1 do
       resources :items
+      resources :profiles
+
+      root 'home#index'
     end
-  end
+  end  
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
