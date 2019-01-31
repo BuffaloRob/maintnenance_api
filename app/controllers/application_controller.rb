39274da1f2677d29da48750f5eeef3e12bbc459d
buffalorob <peckham.rob@gmail.com>
2019-01-31 09:03:28 -0500

    + whitespace for clarity in comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::API
 
   private
   #walkthrough via https://medium.com/devtechtipstricks/build-a-simple-rails-api-server-auth0-jwt-authentication-react-from-scratch-in-30-minutes-or-257cbb2a939a
+  
   # Define unauthorized access json response
   def unauthorized_entity(entity_name)
       render json: { error: "Unauthorized request" }, status:     
